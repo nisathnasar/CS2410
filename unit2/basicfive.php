d295ad7eb2f31923a934d52ce052558da63b4a37
nisath <nisathesharp@Yahoo.com>
2021-02-12 15:45:36 +0000

    first commit

---
+++ unit2/basicfive.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title> Basic PHP Programing </title>
+</head>
+
+<body>
+	<h1>Tasks</h1>
+
+	<!-- Task 1: String-->
+	<!-- write your solution to Task 1 here -->
+	<div class="section">
+		<h2>Task 1 : String</h2>
+		
+	
+	
+	</div>
+
+	<!-- Task 2: Array and image-->
+	<!-- write your solution to Task 2 here -->
+	<div class="section">
+		<h2>Task 2 : Array and image</h2>
+
+		
+		
+	</div>	
+
+	<!-- Task 3: Function definition dayinmonth  -->
+	<!-- write your solution to Task 3 here -->
+	<div class="section">
+		<h2>Task 3 : Function definition</h2>
+		
+	
+	
+	</div>
+	
+
+		
+	<!-- Task 5: Directory operations -->
+	<!-- write your solution to Task 5 here -->
+	<div class="section">
+		<h2>Task 5 : Directory operations</h2>
+
+
+		
+	</div>
+
+	<!-- Task 5 optional: Directory operations -->
+	<!-- write your solution to Task 5 optional here -->
+	<div class="section">
+		<h2>Task 5 optional: Directory operations optional</h2>
+	
+	
+	
+	</div>
+	</div
+
+
+	
+    <!-- Task 4: including external files -->
+	<!-- write your solution to Task 5 here -->
+	<div class="section">
+		<h2>Task 4: including external files</h2>
+			
+			
+	</div>
+
+</body>
+</html>
